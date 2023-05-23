ea9578f137069f85504d0a057295f538f630e79c
Evren Keskin <evrenkeskin0998@gmail.com>
2023-05-23 18:42:32 +0300

    initial commit

---
+++ communication/src/main/java/com/mycompany/communication/Communication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.communication;
+
+/**
+ *
+ * @author evren
+ */
+public class Communication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
